8de2f32b3ca600343aa9e75051caff1f5329a2cb
Tuğrul Yıldırım <yazilimcitugrul@gmail.com>
2022-08-30 02:50:29 +0300

    30 Ağustos 02:50

--- custom.js
+++ custom.js
@@ -19,5 +19,34 @@ const showCategoryButtons =  (categoryName) => {
 }
 
 const showCategoryButtonsMobile =  (categoryName) => {
-    $("#"+categoryName).toggle();
+    let is_displayed = $("#"+categoryName).css("display");
+    if (is_displayed === "none") {
+        $("#mobileCategoryList").children("div").hide();
+        $("#"+categoryName).toggle();
+        $("#mobileProductList").show();
+    }else{
+        $("#mobileCategoryList").children("div").hide();
+        $("#mobileProductList").hide();
+    }
 }
+
+const toggleProductList = () => {
+    $("#mobileProductList").toggle();
+}
+
+$(document).ready(function() {
+    let long_name_td = $(".bg-d9");
+    long_name_td.each(function() {
+        let long_name = $(this).text();
+        console.log(long_name);
+        if (long_name.length > 2) {
+            $(this).css("font-size", "0.7rem");
+            if (long_name.length > 16) {
+                $(this).css("font-size", "0.6rem");
+            }
+            if (long_name.length > 21) {
+                $(this).text(long_name.substring(0, 21) + "..");
+            }
+        }
+    });
+});
